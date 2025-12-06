7f11f781ad80a846b70aac92f99aa30c669cf49d
Santiago De Olivera <santiagodeolivera@gmail.com>
2025-12-06 11:55:40 -0300

    Try to fix AnonymousWalkEmbeddings.AWE object (incomplete)

--- AnonymousWalkEmbeddings.py
+++ AnonymousWalkEmbeddings.py
@@ -21,7 +21,11 @@ import threading
 from collections import Counter
 
 import numpy as np
-import tensorflow as tf
+
+try:
+    import tensorflow.compat.v1 as tf
+except ModuleNotFoundError:
+    import tensorflow as tf
 
 from AnonymousWalkKernel import AnonymousWalks, GraphKernel, Evaluation
 
@@ -147,8 +151,8 @@ class AWE(object):
         self.nodes_per_graphs = dict()
 
         label_suffix = ''
-        if graph_labels is not None:
-            label_suffix = '_' + graph_labels
+        if self.graph_labels is not None:
+            label_suffix = '_' + self.graph_labels
 
         if self.regenerate_corpus == True or not os.path.exists(self.ROOT + self.dataset + '_corpus' + label_suffix):
             if not os.path.exists(self.ROOT + self.dataset + '_corpus' + label_suffix):
@@ -162,7 +166,7 @@ class AWE(object):
                 self.nodes_per_graphs[en] = len(g2v.graph)
 
 
-                g2v.write_corpus(self.neiborhood_size, self.walk_ids, steps, self.graph_labels,
+                g2v.write_corpus(self.neiborhood_size, self.walk_ids, self.steps, self.graph_labels,
                                  self.ROOT + self.dataset + '_corpus{}/{}'.format(label_suffix, self.corpus_fn_name.format(en)))
 
     def _init_graph(self):
@@ -263,7 +267,7 @@ class AWE(object):
             label_suffix = '_' + graph_labels
 
         while True:
-            batch_data, batch_labels = self.g2v.generate_file_batch(batch_size, window_size, self.doc_id,
+            batch_data, batch_labels = self.g2v.generate_file_batch(self.batch_size, self.window_size, self.doc_id,
                                                                     self.ROOT + self.dataset + '_corpus{}/{}'.format(label_suffix, self.corpus_fn_name.format(self.doc_id)),
                                                                     self.nodes_per_graphs[self.doc_id])
             # batch_data, batch_labels = self.g2v.generate_random_batch(batch_size=self.batch_size,
